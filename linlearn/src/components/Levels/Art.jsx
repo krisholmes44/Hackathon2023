9304e3fd6d9cc18994cebbfda081fb62d751bcb0
Kris Holmes <krholmes@lutron.com>
2023-03-26 11:21:22 -0500

    updated styling + added last component

--- linlearn/src/components/Levels/Art.jsx
+++ linlearn/src/components/Levels/Art.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Help from "../commands/commands";
 
 function Art() {
   return (
     <div className="firstRender">
       <img src={require("./PlsWork.png")} alt="PlsWork" />
+      <p>The rules are simple..win</p>
+      <br />
+      <p>You only need 4 commands</p>
+      <Help />
+      <br />
     </div>
   );
 }
---
+++ linlearn/src/components/output/End.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function End() {
+  return (
+    <div className="end">
+      <p>
+        You
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"> win!</a>{" "}
+      </p>
+    </div>
+  );
+}
+
+export default End;
